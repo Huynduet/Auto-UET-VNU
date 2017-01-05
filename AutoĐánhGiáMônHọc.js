8e38b9234f71a3dc7d7230b01d8e6136c1834e67
Huynduet <huynd.backup@gmail.com>
2017-01-05 10:51:47 +0700

    Create AutoĐánhGiáMônHọc.js

---
+++ AutoĐánhGiáMônHọc.js
@@ -0,0 +1,3 @@
+//Auto Đánh Giá Môn Học trên Courses
+//Paste code vào console =))
+list=document.querySelectorAll('input');for(i=list.length-1;i>0;i--){if(list[i].value==4)list[i].click();}list[121].click()
